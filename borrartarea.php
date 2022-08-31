8986bb4dd04ed00ffb30c18f635b4df6f785d3ff
Jesus Garcia Rodriguez <jgr00059@red.ujaen.es>
2022-08-31 20:05:00 +0200

    borrado

--- borrartarea.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'bbdd.php';
-    $id=$_GET['id'];
-
-    $borrartarea=$conexionbbdd->prepare("DELETE FROM tareas WHERE idtarea='$id'");
-    $borrartarea->execute();
-    header('Location: ./usermain.php');
-?>
